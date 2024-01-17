93284c799aa05e62cf51b73fa481431bf792fde2
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-01-17 10:25:16 +0800

    Update ApiDialog.java

--- app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+++ app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
@@ -342,9 +342,9 @@ public class ApiDialog extends BaseDialog {
         URL.DOMAIN_NAME_PROXY = url;      
         // 默认线路地址
         String defaultApiName = "自备份线路";
-        String defaultApi = url + URL.DEFAULT_API_URL;
+        String defaultApi = URL.DEFAULT_API_URL;
         // 默认仓库地址
-        String defaultStoreApi = url + URL.DEFAULT_STORE_API_URL;
+        String defaultStoreApi = URL.DEFAULT_STORE_API_URL;
 
         Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP, new HashMap<>());
         defaultApiMap.put(defaultApiName, defaultApi);
